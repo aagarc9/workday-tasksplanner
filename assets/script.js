63eb8475bd42e4b631532f1525b336fff0865cc1
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-22 10:00:08 -0800

    Update to JS storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
+const inputValue = document.getElementById("input");
+
+const button = document.getElementById("btn");
+
+button.onclick = function () {
+    const text = inputValue.value;
+
+    console.log(text)
+}
+
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
