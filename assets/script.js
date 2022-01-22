579b7570dd94082055f7d18dd4b26c19c84de670
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-22 15:47:15 -0800

    Working on color change v.1

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,29 @@
-const inputValue = document.getElementById("input");
+let inputValue = document.getElementById("input");
+let button = document.getElementById("btn");
+let descr = document.getElementsByClassName("description");
+let  row = document.getElementsByClassName('row');
+let present = document.getElementsByClassName('present');
+let future = document.getElementsByClassName('future');
 
-const button = document.getElementById("btn");
-
-button.onclick = function () {
-    const text = inputValue.value;
-
-    console.log(text)
-}
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+
+function timeColor() {
+    let currentHour = moment().hour()
+
+    if(currentHour < 8) {
+    row.classList.add('past');
+    }
+
+
+
+}
+
+timeColor()
+
+button.onclick = function () {
+    let text = inputValue.value;
+
+    console.log(text)
+}
