a2a843eb0c1d07eecb34e25e8788915e27b6ba38
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-25 07:59:02 -0800

    Updating local storage

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,48 @@ let inputValue = document.getElementById("input");
 let button = document.getElementById("btn");
 let descr = document.getElementsByClassName("description");
 
+// create local storge after user input
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    text = inputValue.value 
+    console.log(text)
 
+    localStorage.setItem('text', text)
+    renderInput()
+});
 
+function renderInput() {
+    let tasks = localStorage.getItem('text');
+    console.log(tasks)
+    descr.textContent = tasks
+}
+// function renderLastRegistered() {
+//     var email = localStorage.getItem("email");
+//     var password = localStorage.getItem("password");
+  
+//     if (!email || !password) {
+//       return;
+//     }
+  
+//     userEmailSpan.textContent = email;
+//     userPasswordSpan.textContent = password;
+//   }
+
+// time and date for header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 // create a function to change description color
 function timeColor() {
     let currentHour = moment().hour()
-
+    console.log(currentHour)
+    if (currentHour > 8) {
+        $(".time8").addClass("past");
+    } else if (currentHour === 8) {
+        $(".time8").addClass("present");
+    } else if (currentHour < 8) {
+        $(".time8").addClass("future");
+    }
     if (currentHour > 9) {
         $(".time9").addClass("past");
     } else if (currentHour === 9) {
@@ -18,18 +51,55 @@ function timeColor() {
     } else if (currentHour < 9) {
         $(".time9").addClass("future");
     }
-     
+    if (currentHour > 10) {
+        $(".time10").addClass("past");
+    } else if (currentHour === 10) {
+        $(".time10").addClass("present");
+    } else if (currentHour < 10) {
+        $(".time10").addClass("future");
+    }
+    if (currentHour > 11) {
+        $(".time11").addClass("past");
+    } else if (currentHour === 11) {
+        $(".time11").addClass("present");
+    } else if (currentHour < 11) {
+        $(".time11").addClass("future");
+    }
+    if (currentHour > 12) {
+        $(".time12").addClass("past");
+    } else if (currentHour === 12) {
+        $(".time12").addClass("present");
+    } else if (currentHour < 12) {
+        $(".time12").addClass("future");
+    }
+    if (currentHour > 13) {
+        $(".time1").addClass("past");
+    } else if (currentHour === 13) {
+        $(".time1").addClass("present");
+    } else if (currentHour < 13) {
+        $(".time1").addClass("future");
+    }
+    if (currentHour > 14) {
+        $(".time2").addClass("past");
+    } else if (currentHour === 14) {
+        $(".time2").addClass("present");
+    } else if (currentHour < 14) {
+        $(".time2").addClass("future");
+    }
+    if (currentHour > 15) {
+        $(".time3").addClass("past");
+    } else if (currentHour === 15) {
+        $(".time3").addClass("present");
+    } else if (currentHour < 15) {
+        $(".time3").addClass("future");
+    }
+    if (currentHour > 16) {
+        $(".time4").addClass("past");
+    } else if (currentHour === 16) {
+        $(".time4").addClass("present");
+    } else if (currentHour < 16) {
+        $(".time4").addClass("future");
+    }
 
 }
-
-
 timeColor()
-
-example = function () {
-    let text = inputValue.value;
-
-    console.log(text)
-}
-function example() {
-    
-}
